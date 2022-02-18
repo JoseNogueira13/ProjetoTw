a2c63bff02178dbfe482e7de51a6855e4803ec8a
JoseNogueira13 <92999470+JoseNogueira13@users.noreply.github.com>
2022-02-18 23:21:21 +0000

    finishedV

--- JS/login.js
+++ JS/login.js
@@ -29,7 +29,7 @@ function openSubscricao() {
 function subscricao() {
     document.getElementById("result").innerHTML = "";
     var sub = document.getElementsByTagName('input');
-      
+    alert("A sua inscrição foi feita com sucesso!")  
     for(i = 0; i < sub.length; i++) {
           
         if(sub[i].type="radio") {
@@ -38,7 +38,25 @@ function subscricao() {
                 document.getElementById("result").innerHTML
                 += " A sua " + sub[i].name + " : "
                 + sub[i].value + "<br>";
+
                     
         }
     }
+}
+var post= document.getElementById("post");
+post.addEventListener("click", function(){
+    var commentBoxValue= document.getElementById("commentBox").value;
+ 
+    var li = document.createElement("li");
+    var text = document.createTextNode(commentBoxValue);
+    li.appendChild(text);
+    document.getElementById("unordered").appendChild(li);
+ 
+});
+function newsletterMessage() { 
+    document.getElementById("newsLetterDiv").style.display = "block";
+}
+function closeNewsletter() { 
+    document.getElementById("newsLetterDiv").style.display = "none";
+    
 }
